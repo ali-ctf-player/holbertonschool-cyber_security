8c870a45a90a5115766232e8c37bd6aaf0e63fc4
ali-ctf-player <eliekberbabayev23082007@gmail.com>
2025-12-15 16:26:17 +0400

    Cyber

--- cybersecurity_basics/0x00_introduction_cybersecurity/2-sha256_validator.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/2-sha256_validator.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-hash=$(sha256sum $1 | awk '{print $1}')
-[ "$hash" = "$2" ] && echo "$1: OK" || echo "$1: FAIL"
+hash=$(sha256sum $1 | awk '{print $1}');[ "$hash" = "$2" ] && echo "$1: OK" || echo "$1: FAIL"
